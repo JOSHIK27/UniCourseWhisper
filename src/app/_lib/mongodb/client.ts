3f3c9c005b6b2d8f4098dd52f93ad7efc9faa07e
JOSHIK <jn2044@hw.ac.uk>
2024-04-10 02:52:37 +0100

    minor fix

--- src/app/_lib/mongodb/client.ts
+++ src/app/_lib/mongodb/client.ts
@@ -13,14 +13,17 @@ const options = {};
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "production"
+) {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    (global as any)._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = (global as any)._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
